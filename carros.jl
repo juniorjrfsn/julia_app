e7915f2aa1e0d7fe37fdfc90f76b83fa6da9d8e0
juniorjrfsn <junior.jrfsn@gmail.com>
2023-08-23 15:29:15 -0400

    soma

--- carros.jl
+++ carros.jl
@@ -9,7 +9,10 @@ carros = [
 	Carro("Volkswagen", "Gol", 	2022),
 	Carro("Hyundai", 	"HB20", 2021)
 ]
-
+println("----------------------")
+println(1+1)
+println('1','1')
+println("----------------------")
 for carro in carros
 	println(carro.nome, carro.modelo, carro.ano)
 end
