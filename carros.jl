5a9b46fdbfb53ed98ef480c96adee38858b816d7
juniorjrfsn <junior.jrfsn@gmail.com>
2023-09-15 16:55:07 -0400

    The diff adds a second print statement to the end of the file, which prints the values of the second item in the carros list. (generated by useblackbox.ai )

--- carros.jl
+++ carros.jl
@@ -16,3 +16,8 @@ println("----------------------")
 for carro in carros
 	println(carro.nome, carro.modelo, carro.ano)
 end
+println("----------------------")
+println("Selecionando o segundo carro")
+segundo_carro = carros[2]
+println("Marca: $(segundo_carro.nome), Modelo: $(segundo_carro.modelo), Ano: $(segundo_carro.ano)")
+println("----------------------")
