da478103319622d84964a8791f7f13b46ccb73eb
juniorjrfsn <70738507+juniorjrfsn@users.noreply.github.com>
2023-09-17 20:21:11 -0400

    The commit removes an unnecessary newline at the end of the file. (generated by useblackbox.ai )

--- calc/main.jl
+++ calc/main.jl
@@ -20,9 +20,10 @@ Peso = pesoN("100.0","Marte");
 # (peso,massa,get_aceleracao_gravitacional(espaco));
 gravidade = Peso[3][1];
 lugar = Peso[3][2];
-println(@sprintf("\nO peso é %.2fN, para a massa de %.2f kg,  a aceleração da gravidade é de  %.8f m/s² %s \n", Peso[1],Peso[2],Peso[3][1],Peso[3][2]));
+println(@sprintf("O peso é %.2fN, para a massa de %.2f kg, a aceleração da gravidade é de  %.8f m/s² %s", Peso[1],Peso[2],Peso[3][1],Peso[3][2]));
 
-println("$gravidade $lugar");
+println("$gravidade $lugar  \n");
+exit()
 
 # executar
 # julia main.jl
