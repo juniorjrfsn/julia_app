ac7e661bbef10a70f6072fee5a0e14be26bbda35
juniorjrfsn <junior.jrfsn@gmail.com>
2023-09-18 15:21:46 -0400

    SQLite and DBInterface packages were added and used to create and populate a SQLite database named "meu_banco.db" with a table named "pessoas" with 3 columns: nome (String), idade (Integer), sexo (String).  (generated by useblackbox.ai )

--- financ/main.jl
+++ financ/main.jl
@@ -1,7 +1,43 @@
 using Printf
 
-include("persist/SQLite_jl/src/SQLite.jl");
+#import Pkg; Pkg.add("WeakRefStrings");
+#import Pkg; Pkg.add("SQLite_jll");
+#import Pkg; Pkg.add("DBInterface");
+
+
+#include("persist/SQLite_jl/src/SQLite.jl");
 
 include("persist/conedatabase.jl");
 include("persist/migration.jl");
 include("persist/mvc.jl");
+import Pkg;
+Pkg.add("SQLite3")
+Pkg.add("DataFrames")
+import Pkg; Pkg.add("SQLite3")
+import SQLite3
+
+# Cria um banco de dados chamado "meu_banco.db"
+db = SQLite3.open("meu_banco.db");
+
+# Cria uma tabela chamada "pessoas"
+db.create_table("pessoas",
+               ["nome", String],
+               ["idade", Integer],
+               ["sexo", String]);
+
+# Insere um registro na tabela "pessoas"
+db.insert("pessoas", ["João", 20, "Masculino"]);
+
+# Insere outro registro na tabela "pessoas"
+db.insert("pessoas", ["Maria", 30, "Feminino"]);
+
+# Lê todos os registros da tabela "pessoas"
+registros = db.query("SELECT * FROM pessoas");
+
+# Imprime os registros
+for registro in registros
+  println(registro);
+end
+
+# Fecha o banco de dados
+db.close();
