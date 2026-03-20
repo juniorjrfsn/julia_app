53660e4f2b1c707208f70b069d5e71b193baacb0
juniorjrfsn <junior.jrfsn@gmail.com>
2026-03-20 15:39:33 -0400

    feat: Add script to replace semicolons with colons in specific segments of lines within a text file.

--- arquivosplit/src/retirar_ponto_e_virgula_de_trecho_de_linha.jl
+++ arquivosplit/src/retirar_ponto_e_virgula_de_trecho_de_linha.jl
@@ -6,33 +6,37 @@ function refatorar_linhas(filepath::String)
     open(output_path, "w") do output
         open(filepath, "r") do file
             for (i, line) in enumerate(eachline(file))
+                # Replace ponto e virgula por dois pontos em trechos de linha
+                # Campos : [ALA-AE-NUMERO-PROCESSO], [INSTRUMENTO-LEGAL], [ALA-AE-HISTORICO], [ALA-AE-CARGO-MT1]
                 if length(line) >= 106 
-                    campo1_inicio = 106
+                    campo1_inicio = 106 # ✅ [ALA-AE-NUMERO-PROCESSO]
                     campo1_fim = min(117, length(line))
                     trecho1 = replace(line[campo1_inicio:campo1_fim], ";" => ":")
 
-                    campo2_inicio = 119
+                    campo2_inicio = 119  # ✅ [INSTRUMENTO-LEGAL]
                     campo2_fim = min(268, length(line))
                     trecho2 = replace(line[campo2_inicio:campo2_fim], ";" => ":")
 
-                    campo3_inicio = 354   # ✅ corrigido
-                    campo3_fim = min(853, length(line))  # ✅ corrigido
+                    campo3_inicio = 354   # ✅ [ALA-AE-HISTORICO]
+                    campo3_fim = min(853, length(line)) 
                     trecho3 = replace(line[campo3_inicio:campo3_fim], ";" => ":")
 
-                    campo4_inicio = 1225   # ✅ corrigido
-                    campo4_fim = min(1234, length(line))  # ✅ corrigido
+                    campo4_inicio = 1225   # ✅ [ALA-AE-CARGO-MT1]
+                    campo4_fim = min(1234, length(line))
                     trecho4 = replace(line[campo4_inicio:campo4_fim], ";" => ":")
 
                     nova_linha = 
-                    line[1:campo1_inicio-1]            * trecho1 *   # ✅ -1 no prefixo
-                    line[campo1_fim+1:campo2_inicio-1] * trecho2 *   # ✅ -1 no prefixo
-                    line[campo2_fim+1:campo3_inicio-1] * trecho3 *   # ✅ -1 no prefixo 
-                    line[campo3_fim+1:campo4_inicio-1] * trecho4 *   # ✅ -1 no prefixo 
+                    line[1:campo1_inicio-1]            * trecho1 *   # ✅ [ALA-AE-NUMERO-PROCESSO]
+                    line[campo1_fim+1:campo2_inicio-1] * trecho2 *   # ✅ [INSTRUMENTO-LEGAL]
+                    line[campo2_fim+1:campo3_inicio-1] * trecho3 *   # ✅ [ALA-AE-HISTORICO]
+                    line[campo3_fim+1:campo4_inicio-1] * trecho4 *   # ✅ [ALA-AE-CARGO-MT1]
                     line[campo4_fim+1:end]
+
                     println(output, nova_linha)
                 else
                     println(output, line)
                 end
+
             end
         end
     end
